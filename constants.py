2ad4cf0ccdfd5143038a45b0ac5d5fa3085d8813
Dwwwarfff <peganovav@gmail.com>
2023-06-29 17:59:38 +0300

    New functions!

---
+++ constants.py
@@ -0,0 +1,25 @@
+PERSON = ['p', 'person']
+QUIT = ['q', 'quit']
+SHELF = ['s', 'shelf']
+FULL_DOC_INFO = ['l']  # ?
+ADD_SHELF = ['ads', 'add shelf']
+DELETE_SHELF = ['ds', 'delete shelf']
+ADD_DOCUMENT = ['ad', 'add document']
+DELETE = ['d', 'delete']
+MOVE = ['m', 'move']
+LIST_OF_COMS = ['lof', 'list of commands']
+
+COMMANDS = {'q': 'to finish the program',
+            'p': 'to show who owns the document',
+            's': 'to show on which shelf the document is held',
+            'l': 'to show full information of the document',
+            'ads': 'to add a new shelf',
+            'ds': 'to delete a shelf',
+            'ad': 'to add a new doc',
+            'd': 'to delete a document',
+            'm': 'to move the document from one shelf to another',
+            'lof': 'to show full list of commands'}
+
+NUMBER = 'number'
+NAME = 'name'
+TYPE = 'type'
---
+++ functions.py
@@ -0,0 +1,39 @@
+documents = [
+    {'type': 'passport', 'number': '2207 876234', 'name': 'Василий Гупкин'},
+    {'type': 'invoice', 'number': '11-2', 'name': 'Геннадий Покемонов'},
+    {'type': 'insurance', 'number': '10006', 'name': 'Аристарх Павлов'}
+]
+
+directories = {
+    '1': ['2207 876234', '11-2'],
+    '2': ['10006'],
+    '3': []
+}
+
+
+def create_doc():
+    pass
+
+
+def create_shelf():
+    pass
+
+
+def read_doc():
+    pass
+
+
+def read_shelf():
+    pass
+
+
+def update_doc():
+    pass
+
+
+def delete_shelf():
+    pass
+
+
+def delete_doc():
+    pass
