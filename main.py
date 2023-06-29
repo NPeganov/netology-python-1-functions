e4ab696b094bab937096aee33c37289ca58df908
Dwwwarfff <peganovav@gmail.com>
2023-06-29 18:44:14 +0300

    Just some beauty changes

--- main.py
+++ main.py
@@ -23,14 +23,14 @@ def factory():
         _type = input('Enter the type of the document \n')
         _owner = input('Enter the name of the document\'s owner \n')
         _shelf = input('Enter the number of a shelf to place the document \n')
-        return create_doc(_num, _type, _owner, _shelf), read_doc_info()
+        return create_doc(_num, _type, _owner, _shelf) + read_doc_info()
     elif command in DELETE:
         doc_to_be_deleted = input('Enter number of a document to delete it: \n')
-        return delete_doc(doc_to_be_deleted), read_doc_info()
+        return delete_doc(doc_to_be_deleted) + read_doc_info()
     elif command in MOVE:
         doc_to_move = input('Enter number of a document to move it: \n')
         _shelf = input('Enter number of the shelf where to move: \n')
-        return update_doc(doc_to_move, _shelf), read_doc_info()
+        return update_doc(doc_to_move, _shelf) + read_doc_info()
     elif command in LIST_OF_COMS:
         return read_commands_list()
     else:
