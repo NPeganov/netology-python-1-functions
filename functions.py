ec6d77bfeb20a6740d742e7e2a7eef4defacc736
Dwwwarfff <peganovav@gmail.com>
2023-06-29 18:42:36 +0300

    Filled the functions

--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
+import logging
+from constants import COMMANDS, NUMBER, TYPE, NAME
+
+
 documents = [
     {'type': 'passport', 'number': '2207 876234', 'name': 'Василий Гупкин'},
     {'type': 'invoice', 'number': '11-2', 'name': 'Геннадий Покемонов'},
@@ -11,29 +15,119 @@ directories = {
 }
 
 
-def create_doc():
-    pass
+def create_shelf(new_num):
+    for _ in directories:
+        if new_num == _:
+            return 'The shelf already exists'
+    directories[new_num] = []
+    shelves = ''
+    last_key = list(directories.keys())[-1]
+    for key in directories:
+        if key == last_key:
+            shelves += f'{key}'
+        else:
+            shelves += f'{key} '
+    return f'The shelf was successfully added. {read_shelf_list()}'
 
 
-def create_shelf():
-    pass
+def create_doc(num, type, owner, shelf):
+    if shelf not in directories:
+        return 'The shelf doesn\'t exist. To add a shelf print \'ads\''
+    owner = owner.split(' ')
+    for word in range(len(owner)):
+        owner[word] = owner[word].capitalize()
+    owner = ' '.join(owner)
+    document = dict()
+    document[TYPE] = type
+    document[NUMBER] = num
+    document[NAME] = owner
+    documents.append(document)
+    directories[shelf].append(num)
+    return 'The document was successfully added!'
 
 
-def read_doc():
-    pass
+def read_doc_owner(doc_num):
+    owner = None
+    for document in documents:
+        if document['number'] == doc_num:
+            owner = document['name']
+            return owner
+    if owner is None:
+        return 'Document owner is not found'
 
 
-def read_shelf():
-    pass
+def read_shelf(doc_num):
+    shelf = None
+    for key, values in directories.items():
+        if doc_num in values:
+            shelf = key
+            return shelf
+    if shelf is None:
+        return 'Document is not found'
 
 
-def update_doc():
-    pass
+def read_doc_info():
+    result = ''
+    for document in documents:
+        for key, values in directories.items():
+            if document['number'] in values:
+                result += f'№: {document["number"]}, type: {document["type"]}, owner: {document["name"]}' \
+                          f', storage shelf: {key} \n'
+    return f'Current list of document: \n{result}'
 
 
-def delete_shelf():
-    pass
+def read_shelf_list():
+    shelves = ''
+    last_key = list(directories.keys())[-1]
+    for key in directories:
+        if key == last_key:
+            shelves += f'{key}'
+        else:
+            shelves += f'{key}, '
+
+    a = f'Current list of shelves: {shelves}' #!
+    return f'Current list of shelves: {shelves}'
 
 
-def delete_doc():
-    pass
+def read_commands_list():
+    the_list_of_coms = str()
+    for key, value in COMMANDS.items():
+        the_list_of_coms += f'\n{key} - {value}'
+    return f'The list of commands: {the_list_of_coms}'
+
+
+def update_doc(doc, shelf):
+    a = None
+    for d in directories.values():
+        if doc in d:
+            d.remove(doc)
+            a = d
+    if a is None:
+        return 'The document wasn\'t found on the shelf'
+    directories[shelf].append(doc)
+    return 'The document was successfully moved!'
+
+
+def delete_shelf(shelf_num):
+    if shelf_num not in directories:
+        return 'The shelf doesn\'t exist'
+    if directories[shelf_num] != list():
+        return 'There are documents on the shelf, remove them before removing the shelf' \
+               ' (print "d" to remove a document).'
+    del directories[shelf_num]
+    return 'The shelf was deleted.'
+
+
+def delete_doc(doc):
+    a = None
+    for document in reversed(range(len(documents))):
+        if documents[document]['number'] == doc:
+            del documents[document]
+            a = document
+    if a is None:
+        return 'The document doesn\'t exist'
+    for d in directories.values():
+        if doc in d:
+            d.remove(doc)
+
+    return 'The document is successfully deleted!'
--- main.py
+++ main.py
@@ -1,49 +1,51 @@
 from constants import *
+from functions import *
+
+
+def factory():
+    command = input('Enter the command, please: \n').lower()
+    if command in PERSON:
+        doc_num = input('Enter number of the document: \n')
+        return read_doc_owner(doc_num)
+    elif command in SHELF:
+        doc_num = input('Enter number of the document: \n')
+        return read_shelf(doc_num)
+    elif command in FULL_DOC_INFO:
+        return read_doc_info()
+    elif command in ADD_SHELF:
+        new_shelf_num = input('Enter number of the new shelf: \n')
+        return create_shelf(new_shelf_num)
+    elif command in DELETE_SHELF:
+        to_be_deleted_shelf_num = input('Enter number of the shelf to delete: \n')
+        return delete_shelf(to_be_deleted_shelf_num) + read_doc_info()
+    elif command in ADD_DOCUMENT:
+        _num = input('Enter the number of a new document: \n')
+        _type = input('Enter the type of the document \n')
+        _owner = input('Enter the name of the document\'s owner \n')
+        _shelf = input('Enter the number of a shelf to place the document \n')
+        return create_doc(_num, _type, _owner, _shelf), read_doc_info()
+    elif command in DELETE:
+        doc_to_be_deleted = input('Enter number of a document to delete it: \n')
+        return delete_doc(doc_to_be_deleted), read_doc_info()
+    elif command in MOVE:
+        doc_to_move = input('Enter number of a document to move it: \n')
+        _shelf = input('Enter number of the shelf where to move: \n')
+        return update_doc(doc_to_move, _shelf), read_doc_info()
+    elif command in LIST_OF_COMS:
+        return read_commands_list()
+    else:
+        return 'This command doesn\'t exist \nPrint \'lof\' to see the list of commands'
 
 
 def user_interaction():
     command = None
     while command not in QUIT:
-        command = input('Enter the command, please: \n').lower()
-        if command in PERSON:
-            doc_num = input('Enter number of the document: \n')
-            print(show_doc_owner(doc_num))
-        elif command in SHELF:
-            doc_num = input('Enter number of the document: \n')
-            print(show_shelf_of_doc(doc_num))
-        elif command in FULL_DOC_INFO:
-            print(show_full_doc_info())
-        elif command in ADD_SHELF:
-            new_shelf_num = input('Enter number of the new shelf: \n')
-            print(add_new_shelf(new_shelf_num))
-        elif command in DELETE_SHELF:
-            to_be_deleted_shelf_num = input('Enter number of the shelf to delete: \n')
-            print(delete_a_shelf(to_be_deleted_shelf_num), clod)
-            print(show_full_doc_info())
-        elif command in ADD_DOC:
-            _num = input('Enter the number of a new document: \n')
-            _type = input('Enter the type of the document \n')
-            _owner = input('Enter the name of the document\'s owner \n')
-            _shelf = input('Enter the number of a shelf to place the document \n')
-            print(add_new_doc(_num, _type, _owner, _shelf), clod)
-            print(show_full_doc_info())
-        elif command in DELETE:
-            doc_to_be_deleted = input('Enter number of a document to delete it: \n')
-            print(delete_doc(doc_to_be_deleted), clod)
-            print(show_full_doc_info())
-        elif command in MOVE:
-            doc_to_move = input('Enter number of a document to move it: \n')
-            _shelf = input('Enter number of the shelf where to move: \n')
-            print(move_doc(doc_to_move, _shelf), clod)
-            print(show_full_doc_info())
-        elif command in LIST_OF_COMS:
-            print(show_list_of_commands())
-        elif command in QUIT:
+        if command in QUIT:  # Tf is going on here
             print('The program stopped')
         else:
-            print('This command doesn\'t exist \nPrint \'lof\' to see the list of commands')
+            print(factory())
 
 
 if __name__ == '__main__':
-    print(show_list_of_commands())
+    print(read_commands_list())
     user_interaction()
